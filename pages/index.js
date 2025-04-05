f224de9e2587425c51edcc7da9d06bf993db7149
jimspi <jpsthesecond@gmail.com>
2025-04-05 09:17:51 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>✨ My AI Projects</h1>
+      <p>This is your portfolio homepage.</p>
+    </div>
+  );
+}
